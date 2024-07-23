8f4340218e94d9e9d6c84ca7aaa3755273dcbbdd
Sergio Ramos <sergioramosdavid@hotmail.com>
2024-07-23 13:06:52 -0400

    error de HEAD solucionado

--- pages/perfil/index.js
+++ pages/perfil/index.js
@@ -5,10 +5,8 @@ import Dashboard from '../../components/Perfil'
 import BanerPerfil from '../../components/Perfil/components/BanerPerfil'
 import { useContext, useEffect } from 'react'
 import UserContext from '../../components/UserContext'
-import Head from 'next/head'
 import Router from 'next/router'
-import Loader from '../../components/Loader'
-import { LoadFile } from '../../components/LoadFile'
+
 export default () => {
   const { user } = useContext(UserContext)
   useEffect(() => {
