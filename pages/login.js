ad41e1b565e63a73b0301b7ccb7417c0326776b6
Sergio Ramos <sergioramosdavid@hotmail.com>
2024-08-04 16:21:23 -0400

    error de login con FB peuba

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,7 @@ import Link from 'next/link'
 import { API_URL } from '../components/Config'
 export default () => {
   var auth2
-  const { signIn } = useContext(UserContext)
+  const { signIn, token } = useContext(UserContext)
   const setUser = (userResponse) => {
     if (userResponse.body.usuario.status !== false) {
       notify.show('Bienvenido 🤩', 'success')
@@ -88,26 +88,28 @@ export default () => {
 
   useEffect(() => {
     startApp()
-    window.fbAsyncInit = function () {
-      FB.init({
-        appId: '333033351546623',
-        xfbml: true,
-        version: 'v20.0',
-      })
-      FB.AppEvents.logPageView()
-    }
-    ;(function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) {
-        return
+    if (window.FB) {
+      window.fbAsyncInit = function () {
+        FB.init({
+          appId: '333033351546623',
+          xfbml: true,
+          version: 'v20.0',
+        })
+        FB.AppEvents.logPageView()
       }
-      js = d.createElement(s)
-      js.id = id
-      js.src = 'https://connect.facebook.net/en_US/sdk.js'
-      fjs.parentNode.insertBefore(js, fjs)
-    })(document, 'script', 'facebook-jssdk')
-  }, [])
+      ;(function (d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0]
+        if (d.getElementById(id)) {
+          return
+        }
+        js = d.createElement(s)
+        js.id = id
+        js.src = 'https://connect.facebook.net/en_US/sdk.js'
+        fjs.parentNode.insertBefore(js, fjs)
+      })(document, 'script', 'facebook-jssdk')
+    }
+  })
   function handlerSubmit() {
     event.preventDefault()
     let headers = new Headers()
