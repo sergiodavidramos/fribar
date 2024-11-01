25b4f8d8133e304e26743074e85a4e6db15f08dc
Sergio Ramos <sergioramosdavid@hotmail.com>
2024-11-01 13:43:55 -0400

    pagina de inicio actualixada 4

--- components/Carrousel/index.js
+++ components/Carrousel/index.js
@@ -1,18 +1,21 @@
 import Loader from '../Loader'
 import { Item } from './Item'
-export const Carrousel = ({ productosDescuento }) => (
-  <div className="main-banner-slider">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="owl-carousel offers-banner owl-theme">
-            {productosDescuento.length > 0 &&
-              productosDescuento.map((producto, index) => (
-                <Item key={index} producto={producto} />
-              ))}
+export const Carrousel = ({ productosDescuento }) => {
+  console.log('--->>', productosDescuento)
+  return (
+    <div className="main-banner-slider">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="owl-carousel offers-banner owl-theme">
+              {productosDescuento.length > 0 &&
+                productosDescuento.map((producto, index) => (
+                  <Item key={index} producto={producto} />
+                ))}
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
